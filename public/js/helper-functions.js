a49f74cc1c7a3dc5a8c6ce5e95438e9737d8f035
samwelkelvin <samwelmunyaka@gmail.com>
2024-10-29 00:42:17 +0300

    Adding public folder with project assets

---
+++ public/js/helper-functions.js
@@ -0,0 +1,54 @@
+
+
+    //make a fetch request 
+ const   getApiData = async (url) => {
+        
+        try {
+                
+            const res = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            }
+            );
+                
+            if (!res.ok) {
+
+                throw new Error(res.statusText);
+
+            }
+            const data = await res.json();
+                
+            return data;
+                
+        } catch (err) {
+                
+            throw new Error(err);
+            
+        }
+                    
+}
+    
+
+ const  splitArray = (inputArray) => {
+
+        const perChunk = 20 // items per chunk    
+
+        return inputArray.reduce((resultArray, item, index) => {
+                    
+            const chunkIndex = Math.floor(index / perChunk)
+
+            if (!resultArray[chunkIndex]) {
+                    
+                resultArray[chunkIndex] = [] // start a new chunk
+
+            }
+
+            resultArray[chunkIndex].push(item);
+
+            return resultArray
+
+        }, [])
+    }
