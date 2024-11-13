86ce4523d66fc7b574e2856ca9ad05aecd4b0604
SAM <samwelmunyaka@gmail.com>
2024-11-13 03:32:50 +0300

    Updated auth functions

--- google-auth-request.js
+++ google-auth-request.js
@@ -4,10 +4,32 @@ const axios = require('axios');
 //import dotenv to read environment variables
 require('dotenv').config();
 
-const tryGoogleAuth = async (code) => {
+const googleAuthentication = {
 
-    try {
+    async getProfileData(access_token){
+
+        try {
+        
+            // Use access_token or id_token to fetch user profile
+            const { data: profile } = await axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
+            
+                headers: { Authorization: `Bearer ${access_token}` },
                 
+            });
+        
+            return profile;
+        
+        } catch (error) {
+        
+            throw new Error(error);
+        
+        }
+        
+    },
+
+ async getAccessToken(authorizationCode){
+    
+        try {
             // Use the Google OAuth 2.0 API to get an access token
 
             // Exchange authorization code for access token
@@ -18,38 +40,24 @@ const tryGoogleAuth = async (code) => {
                 
                 client_secret: process.env.CLIENT_SECRET,
             
-                code,
+                code:authorizationCode,
             
                 redirect_uri: process.env.REDIRECT_URI,
             
                 grant_type: 'authorization_code',
             
             });
-
-
-            //const { access_token, id_token } = data;
-
-        const { access_token } = data;
+    
+            return data;
         
+        } catch (error) {
+        
+            throw new Error(error);
+        
+        }
 
-            // Use access_token or id_token to fetch user profile
-            const { data: profile } = await axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
-            
-                headers: { Authorization: `Bearer ${access_token}` },
-                
-            });
-        
-        console.log(profile);
-        
-        
-        return profile;
 
-    } catch (error) {
-
-        throw new Error(error)
-        
     }
-
 }
 
-module.exports = tryGoogleAuth;
+module.exports = googleAuthentication;
